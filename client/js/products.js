840873b077ae29da2b128e3b2389f5520e95e08a
Alex Martinez <martinez49521@gmail.com>
2024-06-28 13:48:50 -0600

    first commit

---
+++ client/js/products.js
@@ -0,0 +1,23 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Camisa azul",
+    price: 10,
+    quanty: 1,
+    img: "./media/camisa-azul.png",
+  },
+  {
+    id: 2,
+    productName: "Camisa blanca",
+    price: 20,
+    quanty: 1,
+    img: "./media/camisa-blanca.png",
+  },
+  {
+    id: 3,
+    productName: "Camisa negra",
+    price: 30,
+    quanty: 1,
+    img: "./media/camisa-negra.webp",
+  },
+];
